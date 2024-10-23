f289d8f062b97b8884f0ca4315ca42929c1db563
Igor Rodionov <goruha@gmail.com>
2024-10-23 18:00:59 +0200

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,29 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "team_members" {
+  value       = local.enabled ? module.members_merge.*.merged : null
+  description = "Team members"
+}
+
+output "team_name" {
+  value       = local.enabled ? local.team_name : null
+  description = "Team Name"
+}
+
+output "team_id" {
+  value       = local.team_id
+  description = "Team ID"
+}
+
+output "integration" {
+  value       = local.enabled ? module.integration : null
+  description = "Integrations created"
+}
+
+output "routing" {
+  value       = local.enabled ? module.routing : null
+  description = "Routing rules created"
+}
+
+output "escalation" {
+  value       = local.enabled ? module.escalation : null
+  description = "Escalation rules created"
 }
